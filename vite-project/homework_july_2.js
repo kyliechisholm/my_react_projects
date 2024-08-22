c00de36374d14836b0f00c107ac4b4fc511ae45b
kyliechisholm <kyliechisholm90@gmail.com>
2024-08-21 20:57:27 -0600

    first react project

--- vite-project/homework_july_2.js
+++
@@ -1,5 +0,0 @@
-const numbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const expectedOutputEvenNumbers = [2, 4, 6, 8, 10];
-
-const evenNumbers = numbers.filter((number) => number % 2 === 0);
-console.log(evenNumbers);
